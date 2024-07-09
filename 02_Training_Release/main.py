e7b9601650e242b1175517220d4febf56ce77827
THB-account <56601190+THB-account@users.noreply.github.com>
2024-07-09 14:46:12 +0200

    Update main.py

--- 02_Training_Release/main.py
+++ 02_Training_Release/main.py
@@ -1,16 +1,9 @@
-import keyboard
 from model.model import on_start_up, action_loop, isr_state_transition, isr_state_action
 from model.model import on_shutdown
 
 
 
 
-
-no_escape = True
-def esc_key_action(keyboard_event):
-    global no_escape
-    no_escape=False
-keyboard.on_release_key('esc',esc_key_action)
 
 on_start_up()
 try:
