39bbba96888b07666194788db2fb205098905f57
THB-account <56601190+THB-account@users.noreply.github.com>
2024-07-09 14:47:02 +0200

    Update main.py

--- 02_Training_Release/main.py
+++ 02_Training_Release/main.py
@@ -1,7 +1,10 @@
 from model.model import on_start_up, action_loop, isr_state_transition, isr_state_action
 from model.model import on_shutdown
 
+import signal
 
+# just add this to be sure, for shutdownbehavior on KeyboardInterrupt
+signal.signal(signal.SIGINT, on_shutdown)
 
 
 
